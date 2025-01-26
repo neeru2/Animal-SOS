6795a352ccecc8560f6e64128901189b4928120d
MaryamMohamedYahya <MaryamMohamedYahya@users.noreply.github.com>
2025-01-26 07:49:33 +0530

    sql query

--- connection.php
+++ connection.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 $connection = pg_connect("host=localhost dbname=animal user=postgres password=Back-sq01");
 if (!connection){
@@ -16,3 +24,7 @@ while($row = pg_fetch_assoc($result)){
     ";
 }
 ?>
+
+    
+</body>
+</html>
