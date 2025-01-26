a545666b2e45689227d6cb2d5bf1a9f43d32e3bb
MaryamMohamedYahya <MaryamMohamedYahya@users.noreply.github.com>
2025-01-26 07:04:46 +0530

    Display data

--- connection.php
+++ connection.php
@@ -1,3 +1,18 @@
 <?php
-$connection = pg_connect("host=localhost dbname=animal user=postgres password=Back-sq01")
+$connection = pg_connect("host=localhost dbname=animal user=postgres password=Back-sq01");
+if (!connection){
+    echo "Not connected";
+    exit;
+}
+$result = pgquery($connection, "SELECT * FROM animals");
+
+
+while($row = pg_fetch_assoc($result)){
+    echo "
+    <tr>
+    <td>$row[id]</td>
+    <td>$row[type]</td>
+    </tr>
+    ";
+}
 ?>
