36b36f85db0ba16f01edc686f7b89b7c1ebe1deb
MaryamMohamedYahya <MaryamMohamedYahya@users.noreply.github.com>
2025-01-26 07:53:09 +0530

    admin login

--- admin-login.php
+++ admin-login.php
@@ -8,9 +8,9 @@
 <body>
     <div class="container">
         <h1>Admin Login</h1>
-        <form id="adminLoginForm">
-            <input type="text" id="username" placeholder="Username" required>
-            <input type="password" id="password" placeholder="Password" required>
+        <form id="adminLoginForm" action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+            <input type="text" name="username" placeholder="Username" required>
+            <input type="password" name="password" placeholder="Password" required>
             <button type="submit">Login</button>
         </form>
         <a href="index.html" class="btn back-btn">Back to Home</a>
